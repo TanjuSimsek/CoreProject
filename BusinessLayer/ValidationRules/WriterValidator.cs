1baa40369b98c16f87fe1efb0b6680f47745e8ba
TanjuSimsek <tanju.simsek@yandex.com>
2022-02-16 22:50:49 +0300

    first commit

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Soyadı Kısmı Boş Geçilemez");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail Adresi Boş Geçilemez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Girişi Yapın");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş Geçilemez");
+            RuleFor(x => x.WriterPassword).Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.");
+            RuleFor(x => x.WriterPassword).Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.");
+            RuleFor(x => x.WriterPassword).Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.");
+            //Şifre en az bir büyük en az bir küçük karakter ve en az bir sayı olacak
+        }
+    }
+}
---
+++ CoreDemo/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class BlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var model = bm.GetBlogListWithCategory();
+            return View(model);
+        }
+        public IActionResult BlogReadAll(int id) {
+
+            ViewBag.id = id;
+            var values = bm.GetBlogByID(id);
+            return View(values);
+        }
+    }
+}
+//50.derste kaldım..
