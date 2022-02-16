1c77766d655098fbd2092be8630d9883955b861d
TanjuSimsek <tanju.simsek@yandex.com>
2022-02-16 23:22:29 +0300

    ttt

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void BlogAdd(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogDelete(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogUpdate(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetBlogListWithCategory()
+        {
+            return _blogDal.GetListWithCategory();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Blog> GetBlogByID(int id) {
+
+
+            return _blogDal.GetListAll(x => x.BlogID == id);
+        }
+
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+        public List<Blog> GetLast3Blog() {
+
+
+
+            return _blogDal.GetListAll().Take(3).ToList();
+        
+        }
+
+        public List<Blog> GetBlogListByWriter(int id)
+        {
+            return _blogDal.GetListAll(x => x.WriterID == id);
+        }
+    }
+}
---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    //[Authorize]
+    public class WriterController : Controller
+    {
+        //[Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterProfile() {
+
+            return View();
+        }
+        //[Authorize]
+        public IActionResult WriterMail() {
+
+
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Test() {
+
+
+            return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+
+
+            return PartialView();
+
+
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            //53.derste kaldım
+            //Git Deneme
+            //asd
+            return PartialView();
+
+
+        }
+    }
+}
